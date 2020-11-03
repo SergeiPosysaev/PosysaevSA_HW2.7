448e73949eb27045e2776b5be4b23c8efdb3b2f2
Sergei Posysaev <fran@ustlab-1c.ru>
2020-11-03 12:48:18 +0300

    Initial Commit

---
+++ PosysaevSA_HW2.7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PosysaevSA_HW2.7
+//
+//  Created by Sergei Posysaev on 03.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
